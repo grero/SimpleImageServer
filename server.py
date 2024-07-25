30908ed1c5df3c7d27105011ec631863207692d9
Roger Herikstad <roger.herikstad@gmail.com>
2024-07-25 17:15:03 +0800

    Update server.py
    Add event loop and main function

--- server.py
+++ server.py
@@ -1,7 +1,21 @@
 import asyncio
 import websockets
 
-response = f"Received"
-url = "http://localhost/images/image1.png"
-await websocket.send(url)
+async def handle_websocket(websocket, path):
+    try:
+        while True:
+            response = f"Received"
+            url = "http://localhost/images/image1.png"
+            await websocket.send(url)
+          
+    except websockets.ConnectionClosed:
+        pass
 
+
+
+if __name__ == "__main__":
+    # Start the WebSocket server
+    start_server = websockets.serve(handle_websocket, "localhost", 8765)
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
